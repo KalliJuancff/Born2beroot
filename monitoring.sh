0d376e2906d994cc2c691b626d24fbdfe1afd205
JCFF <juancfoofn@yahoo.com>
2024-02-07 11:59:40 +0100

    Update 'monitoring.sh': PRB FIXED with physical CPU's, and add echoes with '-n', and virtual CPU's

--- monitoring.sh
+++ monitoring.sh
@@ -1,6 +1,12 @@
 # La arquitectura de tu sistema operativo y su versión de kernel:
+echo -n "#Architecture: "
 uname -a
 
 # El número de núcleos físicos:
+echo -n "#CPU physical: "
+grep "physical id" /proc/cpuinfo | wc -l
+
+# El número de núcleos virtuales:
+echo -n "#vCPU        : "
 grep "processor" /proc/cpuinfo | tail -n 1 | awk -F' ' '{print $3 + 1}'
 
