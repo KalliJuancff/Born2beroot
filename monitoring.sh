02aafb93dd980a4bdc88e1f218592d9850994125
JC <juancfoofn@yahoo.com>
2024-02-10 12:13:50 +0100

    PRB FIXED in 'CPU load' section, showing percentaje value minus 100

--- monitoring.sh
+++ monitoring.sh
@@ -26,7 +26,7 @@ total_memory=$(echo $total_memory | awk '{printf "%.2f", ($1 / 1024)}')
 formatted_percentage=$(printf "%.2f" $percentage)
 
 # 6. El porcentaje actual de uso de tus núcleos:
-cpu_load=$((100 - (vmstat | tail -1 | awk -F ' ' '{print $15}')))
+cpu_load=$(vmstat | tail -1 | awk -F ' ' '{print $15}')
 cpu_load=$(echo $cpu_load | awk '{printf "%.2f", (100 - $1)}')
 
 # 7. La fecha y hora del último reinicio:
